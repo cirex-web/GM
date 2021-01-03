f51e75cbb371b4a83599e939d09aaa52625eee8c
topshenyi-web <erxu@ctemc.org>
2021-01-03 19:39:57 -0400

    moved graph into function

--- popup/popup.js
+++ popup/popup.js
@@ -53,44 +53,6 @@ window.onload = () => {
             user_database = data.user_database;
             cur_meeting = data.cur_meeting;
             console.log(data);
-            
-            var ctx = document.getElementById('timeGraph').getContext('2d');
-            var timeGraph = new Chart(ctx, {
-                type: 'horizontalBar',
-                data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 3, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
 
 
             if (chrome.runtime.lastError) {
@@ -107,6 +69,7 @@ window.onload = () => {
 
                     updateSpeakerData();
                 }
+                createChart();
             };
         })
     });
@@ -130,6 +93,45 @@ window.onload = () => {
 }
 function updateSpeakerData() {
     $("#test").html(cur_meeting.user_data["AOh14Gix2uH3dS4TB39w7t5AcERHMd1kGObMCMvIT3XTVw=s192-c-mo"].speaking_time);
+}
+function createChart(){
+    var ctx = document.getElementById('timeGraph').getContext('2d');
+    var timeGraph = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 3, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
 chrome.storage.onChanged.addListener(function (changes) {
     for (var key in changes) {
