bac0a6aadd99b925a281d7f5051dee89af5a2832
waguo <warrenguo257@gmail.com>
2021-01-05 14:49:06 -0500

    updating graphs needs fixes with initial coloring also i'll fix the minimum x value later

--- popup/popup.js
+++ popup/popup.js
@@ -4,8 +4,43 @@ let STR = {
 }
 //TODO: deal with page refresh... should refresh data or something
 
+Chart.plugins.register({ //sorting
+    beforeUpdate: function(chart) {
+    if (chart.options.sort) {
+    console.log(chart.data.datasets[0]);
+      let dataArray = chart.data.datasets[0].data.slice();
+      
+      console.log(dataArray);
+      let dataIndexes = dataArray.map((d, i) => i);
+      dataIndexes.sort((a, b) => {
+        return dataArray[a] - dataArray[b];
+      });
+    
+      // sort data array as well
+      dataArray.sort((a, b) => a - b);
+    
+      // At this point dataIndexes is sorted by value of the data, so we 
+    //know how the indexes map to each other
+      let meta = chart.getDatasetMeta(0);
+      let newMeta = [];
+      let labels = chart.data.labels;
+      let newLabels = [];
+    
+      meta.data.forEach((a, i) => {
+        newMeta[dataIndexes[i]] = a;
+        newLabels[dataIndexes[i]] = chart.data.labels[i];
+      });
+    
+      meta.data = newMeta;
+      chart.data.datasets[0].data = dataArray;
+      chart.data.labels = newLabels;
+      }
+      }
+    });
 
 let user_database, cur_meeting, meet_code; //these are the variables that contain the current meeting data (they're updated automatically and updateSpeakerData() is called everytime there's an update.)
+var timeGraph_obj;
+
 /*
 //user_database contains every seen user
 key: image ID, (Generated at time of first appearance)
@@ -58,14 +93,13 @@ window.onload = () => {
                 user_database = data.user_database;
                 cur_meeting = data.cur_meeting;
                 console.log(data);
+                createChart();
                 if (!user_database || !cur_meeting) {
                     $("#test").html("No data yet!");
                 } else {
                     meet_code = cur_meeting.meeting_code;
-
                     updateSpeakerData();
                 }
-                createChart();
             };
         })
     });
@@ -91,34 +125,39 @@ function updateSpeakerData() {
     let str = "";
     for(let [id,user_data] of Object.entries(cur_meeting.user_data)){
         str+=user_database[id].NAME+": "+user_data.speaking_time+"<br>"; 
+        timeGraph_obj.data.datasets[0].data[timeGraph_obj.data.labels.indexOf(user_database[id].NAME)] = user_data.speaking_time
     }
     $("#test").html(str);
+    timeGraph_obj.update();
 }
+
 function createChart(){
+    let label_set = [];
+    let data_set = [];
+    let color_set = [];
+    const fac = new FastAverageColor()
+    for(let [id, user_data] of Object.entries(cur_meeting.user_data)){
+        label_set.push(user_database[id].NAME);
+        data_set.push(user_data.speaking_time);
+        fac.getColorAsync(user_database[id].IMG_ID)
+        .then(color => {
+            color_set.push(color.hex);
+        })
+        .catch(e => {
+            console.log(e);
+        });
+    }
+    // the getColorAsync function is necessary to get the average color of an unloaded image, but the way it is right now results in color_set being an empty set when the graph is made
     var ctx = document.getElementById('timeGraph').getContext('2d');
-    var timeGraph = new Chart(ctx, {
+    timeGraph_obj = new Chart(ctx, {
         type: 'horizontalBar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: label_set,
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 3, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                label: '# of seconds spoken per person',
+                data: data_set,
+                backgroundColor: color_set,
+                borderColor: color_set,
                 borderWidth: 1
             }]
         },
@@ -132,6 +171,9 @@ function createChart(){
             }
         }
     });
+    timeGraph_obj.options.sort = true;
+    timeGraph_obj.update();
+    timeGraph_obj.options.sort = false;
 }
 chrome.storage.onChanged.addListener(function (changes) {
     for (var key in changes) {
